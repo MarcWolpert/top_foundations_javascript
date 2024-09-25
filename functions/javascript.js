a5a93efa3705dd3815ad1b9c9a321a5d7682d770
Marc Wolpert <mewolpert@gmail.com>
2024-09-25 14:55:19 -0700

    feat: Added functions for reusability

---
+++ functions/javascript.js
@@ -0,0 +1,10 @@
+function favoriteAnimal(animal){
+//parameters above
+    return `${animal} is my favorite animal!`
+
+}
+
+//arguments on function call
+console.log(favoriteAnimal('Dog'));
+
+console.log(favoriteAnimal('Dog').replace('favorite','most hated'))
